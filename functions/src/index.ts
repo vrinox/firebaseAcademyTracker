a3e08ae8f4037fc8c7788d018eec81b0e9566c84
vrinox <vrinox@hotmail.com>
2021-10-16 13:03:17 +0200

    1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,17 +4,15 @@ import {createHistoricData} from "./localFunctions/historic";
 import {getAllScholarsFRomDB, updateDB, updateLocalScholars} from "./localFunctions/scholar";
 import {Scholar} from "./models/scholar";
 import {getScholarsOfficialData} from "./localFunctions/officialData";
-import moment = require("moment");
 admin.initializeApp();
 
-const updateScholarData = functions.pubsub.schedule("59 0 * * 0-6").timeZone("Europe/London").onRun(async () => {
+const updateScholarData = functions.pubsub.schedule("0 0 * * 0-6").timeZone("UTC").onRun(async () => {
   let dbScholars:any = await getAllScholarsFRomDB();
   getScholarsOfficialData(dbScholars)
       .then((scholarsNewData:Scholar[])=>{
         dbScholars = updateLocalScholars(dbScholars, scholarsNewData);
         updateDB(dbScholars);
         createHistoricData(dbScholars);
-        console.log("the Update was done successfuly", moment().format());
       });
   return null;
 });
